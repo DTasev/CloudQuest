3928627051e913f9085d7fe70ba375e3d5adc5e0
Dimitar Tasev <dbt@aber.ac.uk>
2016-04-12 10:55:54 +0300

    omg kvadratche

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 /**
  * Created by dimta on 12-Apr-16.
  */
+var FRAMES = 60;
+
 function initialise(){
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext('2d');
+
+    //TODO set keyboard controls
+
+    setInterval(gameLoop(canvas, ctx), 1000/FRAMES);
 }
-function gameLoop(){
+
+function clear(canvas, ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function render(canvas, ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(20, 20, 50, 50);
+    ctx.closePath();
+}
+function handleInput() {
+
+}
+function update() {
     
+}
+function gameLoop(canvas, ctx){
+    // removes last frame
+    clear(canvas, ctx);
+
+    // handles user action
+    handleInput();
+
+    // update the object for the frame
+    update();
+
+    // render objects for the frame on screen
+    render(canvas, ctx);
 }
