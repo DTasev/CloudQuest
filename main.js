0718c06f102296ad4cc5650323e97e7f69b381a9
Dimitar Tasev <dbt@aber.ac.uk>
2016-04-12 11:07:54 +0300

    omg circle

--- main.js
+++ main.js
@@ -2,33 +2,50 @@
  * Created by dimta on 12-Apr-16.
  */
 var FRAMES = 60;
+var i = 30;
 
-function initialise(){
+function initialise() {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext('2d');
 
     //TODO set keyboard controls
 
-    setInterval(gameLoop(canvas, ctx), 1000/FRAMES);
+    //TODO initialise player and obstacles
+
+    return setInterval(function () {
+        gameLoop(canvas, ctx)
+    }, 1000 / 60);
 }
 
 function clear(canvas, ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
-function render(canvas, ctx) {
+function drawSquare(ctx) {
     ctx.beginPath();
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillRect(20, 20, 50, 50);
     ctx.closePath();
 }
+function drawCircle(ctx) {
+    ctx.beginPath();
+    ctx.arc(90, 90, 30, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fill();
+
+}
+function render(canvas, ctx) {
+    drawSquare(ctx);
+    drawCircle(ctx);
+}
 function handleInput() {
 
 }
 function update() {
-    
+
 }
-function gameLoop(canvas, ctx){
+function gameLoop(canvas, ctx) {
+    console.log("Lol");
     // removes last frame
     clear(canvas, ctx);
 
