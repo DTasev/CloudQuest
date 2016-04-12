4239275d7ed81560f8e2a467b70ca5c39e032d16
Dimitar Tasev <dbt@aber.ac.uk>
2016-04-12 17:45:41 +0300

    4:3 k

--- public/main.js
+++ public/main.js
@@ -76,3 +76,72 @@ function gameLoop(canvas, ctx) {
         gameLoop(canvas, ctx)
     }, 1000 / FRAMES);
 })();
+
+var imageLoader = (function(){
+    this.images = [];
+
+    this.loadImages = function () {
+        
+    }
+})
+
+/**
+ * Asset pre-loader object. Loads all images
+ */
+var assetLoader = (function() {
+    // images dictionary
+    var imageArray;
+    this.imgs        = {
+        "bg"            : imageArray
+    };
+    var assetsLoaded = 0;                                // how many assets have been loaded
+    var numImgs      = Object.keys(this.imgs).length;    // total number of image assets
+    this.totalAssest = numImgs;                          // total number of assets
+    /**
+     * Ensure all assets are loaded before using them
+     * @param {number} dic  - Dictionary name ('imgs')
+     * @param {number} name - Asset name in the dictionary
+     */
+    function assetLoaded(dic, name) {
+        // don't count assets that have already loaded
+        if (this[dic][name].status !== "loading" ) {
+            return;
+        }
+        this[dic][name].status = "loaded";
+        assetsLoaded++;
+        // finished callback
+        if (assetsLoaded === this.totalAssest && typeof this.finished === "function") {
+            this.finished();
+        }
+    }
+    /**
+     * Create assets, set callback for asset loading, set asset source
+     */
+    this.downloadAll = function() {
+        var _this = this;
+        var src;
+        // load images
+        for (var img in this.imgs) {
+            if (this.imgs.hasOwnProperty(img)) {
+                src = this.imgs[img];
+                // create a closure for event binding
+                (function(_this, img) {
+                    _this.imgs[img] = new Image();
+                    _this.imgs[img].status = "loading";
+                    _this.imgs[img].name = img;
+                    _this.imgs[img].onload = function() { assetLoaded.call(_this, "imgs", img) };
+                    _this.imgs[img].src = src;
+                })(_this, img);
+            }
+        }
+    }
+
+    return {
+        imgs: this.imgs,
+        totalAssest: this.totalAssest,
+        downloadAll: this.downloadAll
+    };
+})();
+assetLoader.finished = function() {
+    startGame();
+}
